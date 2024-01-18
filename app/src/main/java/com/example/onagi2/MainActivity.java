07a6709d83e56517bf7d92f8a86094cf1eb20a74
Vince <vincentochieng33@gmail.com>
2024-01-18 15:52:23 +0300

    initial commit

--- app/src/main/java/com/example/onagi2/MainActivity.java
+++ app/src/main/java/com/example/onagi2/MainActivity.java
@@ -2,19 +2,16 @@ package com.example.onagi2;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.onagi2.databinding.ActivityMainBinding;
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -22,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
     TextView txtName;
     TextView txtAbout;
     ImageView txtProfilePic;
+    TextView toolbarTitle;
+    ImageView leftIcon;
     BottomNavigationView bottomNavigationView;
-    ActivityMainBinding binding;
+
 
 
 
@@ -33,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notify);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(4);
+
+
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -44,61 +48,31 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.friends:
                         replaceFragment(new FriendsFragment());
-                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,"Friends!",Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.profile:
                         replaceFragment(new ProfileFragment());
-                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,"Profile",Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.notify:
                         replaceFragment(new NotifyFragment());
-                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,"Notifications",Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.messages:
                         replaceFragment(new MessagesFragment());
-                        Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,"Messages",Toast.LENGTH_SHORT).show();
                         break;
                 }
-                return false;
+                return true;
             }
         });
 
 
 
 
-        ImageView leftIcon = findViewById(R.id.left_icon);
-        TextView toolbarTitle = findViewById(R.id.toolbar_title);
-        ImageView rightIcon = findViewById(R.id.right_icon);
 
-        leftIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"back button clicked",Toast.LENGTH_SHORT).show();
-            }
-        });
-        rightIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showMenu(v);
-            }
-            private void showMenu(View v){
-                PopupMenu popupMenu = new PopupMenu(MainActivity.this,v);
-                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getItemId() == R.id.one)
-                            Toast.makeText(MainActivity.this,"First menu clicked",Toast.LENGTH_SHORT).show();
-                        if (item.getItemId() == R.id.two)
-                            Toast.makeText(MainActivity.this,"Second menu clicked",Toast.LENGTH_SHORT).show();
-                        if (item.getItemId() == R.id.three)
-                            Toast.makeText(MainActivity.this,"Third menu clicked",Toast.LENGTH_SHORT).show();
-                        return true;
-                    }
-                });
-                popupMenu.show();
-            }
-        });
+
+
 
 
 
