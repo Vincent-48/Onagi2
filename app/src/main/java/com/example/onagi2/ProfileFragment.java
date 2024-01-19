06ed289beb07924110c670e9c2139c88394082d7
Vince <vincentochieng33@gmail.com>
2024-01-19 11:51:14 +0300

    initial commit

--- app/src/main/java/com/example/onagi2/ProfileFragment.java
+++ app/src/main/java/com/example/onagi2/ProfileFragment.java
@@ -4,11 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 
 public class ProfileFragment extends Fragment {
+
+    ImageView profilepic;
+    EditText usernameInput;
+    EditText phoneInput;
+    Button updateprofileBtn;
+    ProgressBar progressBar;
+    TextView logoutBtn;
 
     View view;
 
@@ -20,11 +32,24 @@ public class ProfileFragment extends Fragment {
 
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profilepic = view.findViewById(R.id.imageprofile);
+        usernameInput = view.findViewById(R.id.profile_username);
+        phoneInput = view.findViewById(R.id.profile_phone);
+        updateprofileBtn = view.findViewById(R.id.profile_update_btn);
+        progressBar = view.findViewById(R.id.profile_progress_bar);
+        logoutBtn = view.findViewById(R.id.log_out_btn);
+
+        getUserData();
+
+        return view;
+
+    }
+    void getUserData(){
+
     }
 }
