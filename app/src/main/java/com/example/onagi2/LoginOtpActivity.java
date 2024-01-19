eb278c8144af9f260247d32559ed638d44141413
Vince <vincentochieng33@gmail.com>
2024-01-19 23:59:35 +0300

    initial commit

--- app/src/main/java/com/example/onagi2/LoginOtpActivity.java
+++ app/src/main/java/com/example/onagi2/LoginOtpActivity.java
@@ -1,28 +1,42 @@
 package com.example.onagi2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class LoginOtpActivity extends AppCompatActivity {
 
     String phoneNumber;
+
+    EditText inputOtp;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView resendOtpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_otp);
 
+        inputOtp = findViewById(R.id.login_otp);
+        nextBtn = findViewById(R.id.login_next_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
+        resendOtpTextView = findViewById(R.id.resend_otp_textview);
+
         phoneNumber = getIntent().getExtras().getString("phone");
         Toast.makeText(getApplicationContext(),phoneNumber,Toast.LENGTH_SHORT).show();
 
-        Map<String,String> data = new HashMap<>();
-        FirebaseFirestore.getInstance().collection("test").add(data);
+
+    }
+    void sendOtp(String phoneNumber, boolean isResend){
+
+    }
+    void setInProgress(boolean inProgress){
+        if(inProgress)
     }
 }
