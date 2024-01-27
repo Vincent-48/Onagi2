7a97f460f0470145f00dd8d4179d22332ef98dec
Vince <vincentochieng33@gmail.com>
2024-01-27 23:52:37 +0300

    initial commit

--- app/src/main/java/com/example/onagi2/SearchUserActivity.java
+++ app/src/main/java/com/example/onagi2/SearchUserActivity.java
@@ -1,14 +1,46 @@
 package com.example.onagi2;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchUserActivity extends AppCompatActivity {
+
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+
+        searchInput = findViewById(R.id.search_user_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recyler_view);
+
+        searchInput.requestFocus();
+
+        backButton.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if (searchTerm.isEmpty() || searchTerm.length() < 3){
+                searchInput.setError("Invalid Username");
+                System.out.println("Enter valid username");
+                return;
+            }
+            setupSearchRecyclerView(searchTerm);
+        });
+    }
+    void setupSearchRecyclerView(String searchTerm){
+
     }
 }
