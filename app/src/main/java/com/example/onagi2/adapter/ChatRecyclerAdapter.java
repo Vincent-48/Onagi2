8618edee8fe2c4723e26cc9f822466df1f1a4aa9
Vince <vincentochieng33@gmail.com>
2024-02-04 15:36:53 +0300

    initial commit

---
+++ app/src/main/java/com/example/onagi2/adapter/ChatRecyclerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.onagi2.adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.onagi2.Model.ChatMessageModel;
+import com.example.onagi2.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class ChatRecyclerAdapter extends FirestoreRecyclerAdapter<ChatMessageModel, ChatRecyclerAdapter.ChatModelViewHolder> {
+
+    Context context;
+    public ChatRecyclerAdapter(@NonNull FirestoreRecyclerOptions<ChatMessageModel> options, Context context) {
+        super(options);
+        this.context =context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ChatModelViewHolder holder, int position, @NonNull ChatMessageModel model) {
+
+
+    }
+
+    @NonNull
+    @Override
+    public ChatModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.chat_message_recycler_row,parent,false);
+        return new ChatModelViewHolder(view);
+    }
+
+    class ChatModelViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ChatModelViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+        }
+    }
+}
+
