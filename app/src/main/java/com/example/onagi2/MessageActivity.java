39c05035395f7d1b401b0a2d2e33b7bd68d3db71
Vince <vincentochieng33@gmail.com>
2024-02-06 23:13:16 +0300

    initial commit

--- app/src/main/java/com/example/onagi2/MessageActivity.java
+++ app/src/main/java/com/example/onagi2/MessageActivity.java
@@ -102,6 +102,7 @@ public class MessageActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<DocumentReference> task) {
                         if (task.isSuccessful()){
+                            System.out.println("******");
                             messageInput.setText("");
                         }
                     }
