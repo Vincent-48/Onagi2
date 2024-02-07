b1e9e41e90197334fc78746ab82725d9b903ce9f
Vince <vincentochieng33@gmail.com>
2024-02-07 23:58:17 +0300

    initial commit

--- app/src/main/java/com/example/onagi2/MessagesFragment.java
+++ app/src/main/java/com/example/onagi2/MessagesFragment.java
@@ -1,15 +1,17 @@
 package com.example.onagi2;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 public class MessagesFragment extends Fragment {
+
+    RecyclerView recyclerView;
 
     View view;
 
@@ -22,6 +24,7 @@ public class MessagesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_messages, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view);
         return view;
     }
 }
